0fa8a0b8fa6ad5edaa13128fad6a101f0a98dcab
Gurvansh Singh <gurvansh007@gmail.com>
2026-03-29 21:21:06 -0400

    test commit

--- src/main/java/com/gurvansh/jobtracker/controller/IndexController.java
+++ src/main/java/com/gurvansh/jobtracker/controller/IndexController.java
@@ -1,5 +1,9 @@
 package com.gurvansh.jobtracker.controller;
 
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,6 +13,11 @@ public class IndexController {
   @GetMapping("/")
   public String index() {
     return "index";
+  }
+
+  @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> hello() {
+      return ResponseEntity.ok(Map.of("message", "Hello World"));
   }
 
 }
